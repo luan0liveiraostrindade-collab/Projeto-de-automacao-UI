9bd66ee933ba3a1c9750cbbf9f2c4e93bc56596a
Luan Trindade <luan.os.trindade@gmail.com>
2026-06-19 23:21:52 -0300

    automação dos testes de erro

--- cypress/e2e/automacaoui.cy.js
+++ cypress/e2e/automacaoui.cy.js
@@ -1,24 +1,53 @@
 describe('Funcionalidade: login', () => {
 
-it.only('Deve fazer login com sucesso',() => {
-cy.visit('localhost:3000/login.html')
-cy.get('#email').type('admin@biblioteca.com')
-cy.get('#password').type('admin123')
-cy.get('#login-btn').click()
-cy.contains('Painel Administrativo').should('be.visible')
-});
-
-it('Teste 2', () => {
-    
-});
-
-it('teste 3', () => {
-    
-});
+    beforeEach(() => {
+        cy.visit('login.html')
+    });
 
 
+    it('Deve fazer login com sucesso', () => {
+        cy.get('#email').type('admin@biblioteca.com')
+        cy.get('#password').type('admin123')
+        cy.get('#login-btn').click()
+        cy.url().should('include', '/admin-dashboard.html')
+        cy.contains('Painel Administrativo').should('be.visible')
+    });
+
+    //Testes de erro
+
+    it('Não deve efetuar login sem preencher o campo email', () => {
+        cy.get('#password').type('admin123')
+        cy.get('#login-btn').click()
+        cy.contains('Por favor, insira um email válido.').should('be.visible')
+    });
+
+    it('Não deve efetuar login sem preencher o campo senha', () => {
+        cy.get('#email').type('admin@biblioteca.com')
+        cy.get('#login-btn').click()
+        cy.get('#password').should('have.class', 'is-invalid')
+    });
+
+    it('Não deve efetuar login ao errar o campo email', () => {
+        cy.get('#email').type('admin@biblioteca')
+        cy.get('#password').type('admin123')
+        cy.get('#login-btn').click()
+        cy.contains('Por favor, insira um email válido.').should('be.visible')
+
+    });
+
+    it('Não deve efetuar login ao errar o campo senha', () => {
+        cy.get('#email').type('admin@biblioteca.com')
+        cy.get('#password').type('123')
+        cy.get('#login-btn').click()
+        cy.contains('Email ou senha incorretos.').should('be.visible')
+    });
+
+    //Massa de dados fake
 
 
 
-    
+
+
+
+
 });
